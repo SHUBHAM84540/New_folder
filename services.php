c4271354448353d25a5e8a1bf87f2abab11e6d95
MR.Shubh <shubhampala84@gmail.com>
2025-04-21 16:21:59 +0530

    Create services.php
    services pages

---
+++ services.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html> 
+<html lang="en"> 
+<head> 
+    <meta charset="UTF-8"> 
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <title>PHP Dynamic Website</title> 
+    <link rel="stylesheet" href="css/style.css"/> 
+</head> 
+ 
+<body> 
+ 
+    <!-- -----------NAVIGATION--------- --> 
+    <section class="main"> 
+      <nav> 
+        <a href="#" class="logo"> 
+           <img src="image/logo1.jpg"/> 
+        </a> 
+         
+        <ul class="menu"> 
+           <li><a href="index.php" >Home</a></li> 
+           <li><a href="services.php" class="active">Services</a></li> 
+           <li><a href="contact.php">Contact</a></li> 
+           <li><a href="register.php">Register</a></li> 
+        </ul> 
+      </nav> 
+ 
+      
+  
+    <!-- ---------------- SERVICES BOXES----------------> 
+    <section class="services"> 
+        <div class="service-container"> 
+ 
+            <div class="service-box"> 
+                <div class="f-img"> 
+                     <img src="image/body.jpg"/> 
+                </div> 
+                <div class="f-text"> 
+                     <h4>Body Building</h4>                      <p>Get best Trainers</p> 
+                     <p>6AM to 9PM</p> 
+ 
+                      
+                </div> 
+            </div> 
+            <div class="service-box"> 
+                <div class="f-img"> 
+                     <img src="image/yoga2.jpg"/> 
+                </div> 
+                <div class="f-text"> 
+                     <h4>Yoga</h4> 
+                     <p>Calm your body and mind </p> 
+                     <p>6AM to 8AM</p> 
+                      
+                      
+                </div> 
+            </div> 
+            <div class="service-box"> 
+                <div class="f-img"> 
+                     <img src="image/zumba2.jpg"/> 
+                </div> 
+                <div class="f-text"> 
+                     <h4>Zumba</h4> 
+                     <p>Workout with Fun</p> 
+                     <p>7AM to 9AM </p> 
+                      
+                </div> 
+            </div> 
+ 
+        </div> 
+    </section> 
+</body> 
+</html> 
