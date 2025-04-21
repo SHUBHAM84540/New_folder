98ecfe600439a89c97ef0ed3a1c64a39b33c0938
MR.Shubh <shubhampala84@gmail.com>
2025-04-21 16:23:19 +0530

    Create contact.php
    contactpages

---
+++ connect.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html> 
+<html lang="en"> 
+<head> 
+    <meta charset="UTF-8"> 
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <title>PHP Dynamic Website</title> 
+    <link rel="stylesheet" href="css/style.css"/> 
+</head> 
+<body> 
+ <!-- -----------NAVIGATION--------- --> 
+    <section class="main"> 
+      <nav> 
+        <a href="#" class="logo"> 
+           <img src="image/logo1.jpg"/> 
+        </a> 
+        <ul class="menu"> 
+           <li><a href="index.php" >Home</a></li> 
+           <li><a href="services.php" rel="stylesheet" type="text/css">Services</a></li> 
+           <li><a href="contact.php" class="active">Contact</a></li> 
+           <li><a href="register.php">Register</a></li> 
+        </ul> 
+      </nav> 
+ <!-- ---------------CONTACT------------- --> 
+    <section class="contact"> 
+        <div class="contact-container"> 
+           <div class="contact-box"> 
+                <div class="c-img"> 
+                     <img src="image/phone.png"/> 
+                </div> 
+                <div class="c-text"> 
+                     <h4>Call Us at</h4> 
+                     <p>+91 7898887670</p> 
+                    </div> 
+            </div> 
+            <div class="contact-box"> 
+                <div class="c-img"> 
+                     <img src="image/mail.jpg"/> 
+                </div> 
+                <div class="c-text"> 
+                     <h4>Mail Us at</h4> 
+                     <p>fitgym@gmail.com</p> 
+                    </div> 
+            </div> 
+            <div class="contact-box"> 
+                <div class="c-img"> 
+                     <img src="image/address.png"/> 
+                </div> 
+                <div class="c-text"> 
+                     <h4>Visit Us</h4> 
+                     <p>Gulabra,gali-1</p> 
+                      </div>             </div> 
